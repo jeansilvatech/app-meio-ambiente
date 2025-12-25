ca5de7a5d7ec5077d73a8027cea8347d4dfb20c5
jeansilvatech <jeanpsilva10@gmail.com>
2025-12-24 22:40:06 -0300

    [feat] tratamento para não aparecer cards com 0 focos

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -7,11 +7,15 @@ interface CardProps{
 const Card = ({ local, quantidade}:CardProps)=>{
     return(
         <>
-        <div key={local} data-aos="fade-right" className="lg:w-[300px] lg:h-[300px] w-[150px] h-[150px] bg-[#71E4C0] text-black flex justify-center items-center flex-col shadow-2xl m-2 rounded-2xl border border-black/30">
-                <h2 className="uppercase text-center text-wrap">{local}</h2>
-                <p className="font-bold text-3xl lg:text-6xl">{quantidade}</p>
-                <span>focos</span>      
-        </div>
+        {
+        quantidade > 0 &&(
+            <div key={local} data-aos="fade-right" className="lg:w-[300px] lg:h-[300px] w-[150px] h-[150px] bg-[#71E4C0] text-black flex justify-center items-center flex-col shadow-2xl m-2 rounded-2xl border border-black/30">
+                    <h2 className="uppercase text-center text-wrap">{local}</h2>
+                    <p className="font-bold text-3xl lg:text-6xl">{quantidade}</p>
+                    <span>focos</span>      
+            </div>
+            )
+        }
         </>
     )
 }
